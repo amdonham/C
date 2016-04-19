d342798d6ed44e3476d35e276d6c571d11304297
amdonham <amdonham@crimson.ua.edu>
2016-04-18 19:52:06 -0500

    Page Table Working

--- project5.c
+++ project5.c
@@ -6,6 +6,33 @@
 long numAdds = 0; 
 int *page;
 int *offset;
+long *pageTable;
+long* physical;
+
+
+void getPhysical(long *logicals){
+	physical = malloc(sizeof(long)*1000);
+	int i;
+	long frame = 0;
+	for(i = 0;i<numAdds;i++){
+		int off = offset[i];
+		if(pageTable[page[i]] == 1){
+			pageTable[page[i]] = frame; 
+			frame += 256;
+		}
+		int frameNum = pageTable[page[i]];
+		physical[i] = frameNum + off;
+	}
+}
+
+void initPageTable(){
+	pageTable = malloc(256*sizeof(long));
+	int i;
+	pageTable[0] = 0;
+	for(i = 1; i < 256;i++){
+		pageTable[i] = 1;
+	}
+}
 
 void toDec(int *bin,int index){
 	int dec = 0;
@@ -31,8 +58,8 @@ void toDec(int *bin,int index){
 
 void toBinary(long *adds){
 	int i;
-	page = malloc(65536*numAdds);
-	offset = malloc(65536*numAdds);
+	page = malloc(sizeof(long)*numAdds);
+	offset = malloc(sizeof(long)*numAdds);
 
 	for( i = 0; i < numAdds; i++){
 		long num = adds[i];
@@ -52,7 +79,7 @@ long * openFile(char* filename){
 	//Open file
 	FILE *fp;
 	fp = fopen(filename,"r");
-	long *addresses = malloc(65536*1000);
+	long *addresses = malloc(sizeof(long)*1000);
 	if(fp==NULL){printf("File not found.\n");}
 	else{
 
@@ -83,15 +110,15 @@ int main(int argc,char *argv[]){
 	//Open file and read addresses
 	long *logicals = openFile(addresses);
 	toBinary(logicals);
-	
-	
+	initPageTable();
+	getPhysical(logicals);
 	//Print out logical addresses
 	int i;
 	for(i = 0; i < numAdds;i++){
 		printf("%lu : ",logicals[i]);
 		printf("%d ",page[i]);
 
-		printf("%d",offset[i]);
+		printf("%lu",physical[i]);
 		printf("\n");
 		
 	}
